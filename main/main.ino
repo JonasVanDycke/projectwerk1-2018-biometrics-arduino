aebf928bb734870247987126b5eb17216a742537
Reavershark <jonasty1999@gmail.com>
2018-03-30 16:37:11 +0200

    Make a version that creates dummy data

--- main/main.ino
+++ main/main.ino
@@ -1,25 +1,20 @@
-#include <LiquidCrystal.h>
-
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-
-double value1 = 100.00;
-double value2 = 123;
+const int numberOfValues = 5;
+double values[numberOfValues] = {25.00, 0, 2, -9.81, 85.2};
+char startChar = '[';
+char separatorChar = ';';
+char endChar = ']';
+int delayTime = 1000;
 
 void setup() {
   Serial.begin(115200);
-  lcd.begin(16, 2);
 }
 
-void loop() {
-  lcd.setCursor(0,0);
-  lcd.print("Value1: " + String(value1));
-  lcd.setCursor(0,1);
-  lcd.print("Value2: " + String(value2));
-  
-  Serial.print(String("[") + value1 + String("|") + value2 + String("]"));
-  Serial.print("\n");
-  delay(500);
-
-  value1++;
-  value2++;
+void loop() {  
+  Serial.print(startChar);
+  for(int i = 0; i < numberOfValues; i++) {
+    Serial.print(values[i] + separatorChar);
+    values[i]++;
+  }
+  Serial.print(endChar);
+  delay(delayTime);
 }
