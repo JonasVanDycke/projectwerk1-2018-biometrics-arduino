abb50a0629f203aa08c307ea831f5fafd8a46487
Reavershark <jonasty1999@gmail.com>
2018-03-30 08:25:24 +0200

    Added include for LCD

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,9 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd();
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
