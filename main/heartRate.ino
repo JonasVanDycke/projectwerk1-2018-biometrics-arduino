a86751bb1a42015ced702a2d11e2e6eeb8c76e82
unknown <jonas.vandycke@student.vives.be>
2018-04-20 17:18:27 +0200

    refactored heartrate and values

--- main/heartRate.ino
+++ main/heartRate.ino
@@ -1,24 +1,17 @@
 //Video link:  https://www.youtube.com/watch?v=82T_zBZQkOE
-double alpha = 0.75;
-static double oldValue = 0;
-static double oldChange = 0;
 
 double getHeartRate() {
-    int rawValue = analogRead (PulseSensor);                                         // Reading the sensors values
-    double value = alpha * oldValue + (1 - alpha) * rawValue;         // calculating values using the formula
-    int x = rawValue/5;
-    int value2 = 6000/x;
-    oldValue = value;
-    
-    return value;
+  int rawValue = analogRead (PulseSensor);
+  int heartRate = rawValue/5;
+  int bpm = 6000/heartRate;
 
-    lcd.setCursor(0,0);
-    lcd.print("Heart Rate: ");
-    lcd.print(rawValue);
-    lcd.setCursor(0,1);
-    lcd.print("BPM: ");
-    lcd.print(value2);
-    
-    delay(delayTime);
- }
-
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Heart Rate: ");
+  lcd.print(heartRate);
+  lcd.setCursor(0,1);
+  lcd.print("BPM: ");
+  lcd.print(bpm);
+  
+  return heartRate;
+}
