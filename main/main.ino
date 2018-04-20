7ff4ef7aaa270f7b2ccad82d7ade490bea152171
Reavershark <jonasty1999@gmail.com>
2018-04-20 15:09:55 +0200

    Random generator

--- main/main.ino
+++ main/main.ino
@@ -7,14 +7,15 @@ int delayTime = 1000;
 
 void setup() {
   Serial.begin(115200);
+  randomSeed(analogRead(0));
 }
 
-void loop() {  
+void loop() {
   Serial.print(startChar);
   for(int i = 0; i < numberOfValues; i++) {
     Serial.print(values[i]);
     Serial.print(separatorChar);
-    values[i]++;
+    values[i] = values[i] + random(-3,3);
   }
   Serial.print(endChar);
   delay(delayTime);
